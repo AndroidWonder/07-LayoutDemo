69787fb6353def062386c58288df18991b6951da
ClassroomAccount <jpepe44@gmail.com>
2019-12-18 19:52:56 -0500

    update rev

--- app/src/main/java/com/course/example/LayoutDemo.java
+++ app/src/main/java/com/course/example/LayoutDemo.java
@@ -9,10 +9,10 @@ public class LayoutDemo extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.main_linearlayout);
+        setContentView(R.layout.main_linearlayout);
         //setContentView(R.layout.main_relative);
        // setContentView(R.layout.main_gridlayout);
-        setContentView(R.layout.main_tablelayout);
+        //setContentView(R.layout.main_tablelayout);
         //setContentView(R.layout.main_equalcolumn);
         //setContentView(R.layout.main_scrollview);
         //setContentView(R.layout.main_absolutelayout);
